a1bc3a61f83b6757d59b8b3f47314277bca77c29
nazarov <nezerovabuzar@gmail.com>
2025-01-03 18:51:28 +0400

    last changes for patterns

--- design_patterns/creational_patterns/abstract_factory.py
+++ design_patterns/creational_patterns/abstract_factory.py
@@ -1,91 +1,88 @@
+"""The idea behind Abstract Factory Pattern is very similar to the normal Factory Pattern, the only difference being that all of the factories have multiple separate methods for creating objects, and the kind of factory is what determines the family of objects."""
+
 from abc import ABC, abstractmethod
 
 
-class Creator(ABC):
-    """
-    The Creator class declares the factory method that is supposed to return an
-    object of a Product class. The Creator's subclasses usually provide the
-    implementation of this method.
-    """
-
-    @abstractmethod
-    def factory_method(self):
-        """
-        Note that the Creator may also provide some default implementation of
-        the factory method.
-        """
-        pass
-
-    def some_operation(self):
-        """
-        Also note that, despite its name, the Creator's primary responsibility
-        is not creating products. Usually, it contains some core business logic
-        that relies on Product objects, returned by the factory method.
-        Subclasses can indirectly change that business logic by overriding the
-        factory method and returning a different type of product from it.
-        """
-
-        product = self.factory_method()
-        result = f"Creator: The same creator's code has just worked with {product.operation()}"
-        return result
-
-
-class ConcreateCreater1(Creator):
-    """
-    Note that the signature of the method still uses the abstract product type,
-    even though the concrete product is actually returned from the method. This
-    way the Creator can stay independent of concrete product classes.
-    """
-
-    def factory_method(self) -> Product:
-        return ConcreteProduct1()
-
-
-class ConcreteCreator2(Creator):
-    def factory_method(self) -> Product:
-        return ConcreteProduct2()
-
-
 class Product(ABC):
-    """
-    The Product interface declares the operations that all concrete products
-    must implement.
-    """
 
     @abstractmethod
-    def operation(self) -> str:
+    def cook(self):
         pass
 
 
-"""
-Concrete Products provide various implementations of the Product interface.
-"""
+class FettuccineAlfredo(Product):
+    name = "Fettuccine Alfredo"
+
+    def cook(self):
+        print("Italian main course prepared: " + self.name)
 
 
-class ConcreteProduct1(Product):
-    def operation(self) -> str:
-        return "{Result of the ConcreteProduct1}"
+class Tiramisu(Product):
+    name = "Tiramisu"
+
+    def cook(self):
+        print("Italian dessert prepared: " + self.name)
 
 
-class ConcreteProduct2(Product):
-    def operation(self) -> str:
-        return "{Result of the ConcreteProduct2}"
+class DuckALOrange(Product):
+    name = "Duck À L'Orange"
+
+    def cook(self):
+        print("French main course prepared: " + self.name)
 
 
-def client_code(creator: Creator) -> None:
-    """
-    The client code works with an instance of a concrete creator, albeit through
-    its base interface. As long as the client keeps working with the creator via
-    the base interface, you can pass it any creator's subclass.
-    """
+class CremeBrulee(Product):
+    name = "Crème brûlée"
 
-    print(f"Client: I'm not aware of the creator's class, but it still works.\n"
-          f"{creator.some_operation()}", end="")
+    def cook(self):
+        print("French dessert prepared: " + self.name)
 
-if __name__ == "__main__":
-    print("App: Launched with the ConcreteCreator1.")
-    client_code(ConcreateCreater1())
-    print("\n")
 
-    print("App: Launched with the ConcreteCreator2.")
-    client_code(ConcreteCreator2())
+class Factory(ABC):
+
+    @abstractmethod
+    def get_dish(type_of_meal):
+        pass
+
+
+class ItalianDishesFactory(Factory):
+    def get_dish(type_of_meal):
+        if type_of_meal == "main":
+            return FettuccineAlfredo()
+        if type_of_meal == "dessert":
+            return Tiramisu()
+
+    def create_dessert(self):
+        return Tiramisu()
+
+
+class FrenchDishesFactory(Factory):
+    def get_dish(type_of_meal):
+        if type_of_meal == "main":
+            return DuckALOrange()
+
+        if type_of_meal == "dessert":
+            return CremeBrulee()
+
+
+class FactoryProducer:
+    def get_factory(self, type_of_factory):
+        if type_of_factory == "italian":
+            return ItalianDishesFactory
+        if type_of_factory == "french":
+            return FrenchDishesFactory
+
+
+fp = FactoryProducer()
+
+fac = fp.get_factory("italian")
+main = fac.get_dish("main")
+main.cook()
+dessert = fac.get_dish("dessert")
+dessert.cook()
+
+fac1 = fp.get_factory("french")
+main = fac1.get_dish("main")
+main.cook()
+dessert = fac1.get_dish("dessert")
+dessert.cook()
--- design_patterns/creational_patterns/factory_method.py
+++ design_patterns/creational_patterns/factory_method.py
@@ -1,3 +1,11 @@
+"""Factories are used to encapsulate the information about classes we're using while instantiating them based on certain parameters we provide them with.
+
+By using a factory, we can switch out an implementation with another by simply changing the parameter that was used to decide the original implementation in the first place.
+
+This decouples the implementation from the usage in such a way that we can easily scale the application by adding new implementations and simply instantiating them through the factory - with the exact same codebase.
+
+If we just get another factory as a parameter, we don't even need to know which class it produces. We just need to have a uniform factory method that returns a class guaranteed to have a certain set of behaviors. """
+
 from abc import ABC, abstractmethod
 
 
